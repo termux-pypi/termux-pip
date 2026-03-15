4bef45bced066c5c98b7e1f974bb896879d647ed
Loamfy <loamfy.tools@gmail.com>
2026-03-15 20:39:22 +0500

    feat: add flush=True for logs

--- src/tpip/utils.py
+++ src/tpip/utils.py
@@ -4,15 +4,15 @@ import sys
 
 
 def log_info(message: str):
-    print(f'{Colors.OKBLUE}[INFO]{Colors.ENDC} {message}')
+    print(f'{Colors.OKBLUE}[INFO]{Colors.ENDC} {message}', flush=True)
 
 
 def log_success(message: str):
-    print(f'{Colors.OKGREEN}[SUCCESS]{Colors.ENDC} {message}')
+    print(f'{Colors.OKGREEN}[SUCCESS]{Colors.ENDC} {message}', flush=True)
 
 
 def log_error(message: str):
-    print(f'{Colors.FAIL}[ERROR]{Colors.ENDC} {message}', file=sys.stderr)
+    print(f'{Colors.FAIL}[ERROR]{Colors.ENDC} {message}', file=sys.stderr, flush=True)
 
 
 def run_command(command, shell=False):
